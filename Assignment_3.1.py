e2357a5b8a4bcc27cb1a6fd248ca57cf2fd18e8b
josephh27 <josephjasonsbuhain@iskolarngbayan.pup.edu.ph>
2021-10-31 20:58:18 +0800

    Assign_3.1

--- Assignment_3.1.py
+++ Assignment_3.1.py
@@ -1,21 +1,21 @@
 
 def applesBought():
-    myApples = int(input("How many apples do you wish to buy? "))
+    myApples = int(input("How many apples do you wish to buy? ")) #This is where the program gets informed on how many apples will be bought
     return myApples
 
 def orangesBought():
-    myOranges = int(input("How many oranges do you wish to buy? "))
+    myOranges = int(input("How many oranges do you wish to buy? ")) #This is the information on how many oranges will be bought by the user
     return myOranges
 
 def totalPrice():
-    totalCost = myApples*20 + myOranges*25
+    totalCost = myApples*20 + myOranges*25         #This function will calculate the total cost of the apples and oranges that the user tried to buy
     return totalCost
 
 def postShopping(myApples, myOranges, myPrice):
-    print(f'{myApples}(20) + {myOranges}(25) = {myPrice}')
+    print(f'{myApples}(20) + {myOranges}(25) = {myPrice}')    #This is the part where we show visibility of the price calculations so that you are assured we didn't pickpocket your money
 
 def thankYou():
-    print("Please come back again before we go bankrupt")
+    print("Please come back again before we go bankrupt")    #A reminder for the user
 
 myApples = applesBought()
 myOranges = orangesBought()
