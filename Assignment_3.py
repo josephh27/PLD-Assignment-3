84cee3b370a465ac538c69c6f79ecbfaf411004a
josephh27 <josephjasonsbuhain@iskolarngbayan.pup.edu.ph>
2021-10-31 20:03:10 +0800

    Assign_3

--- Assignment_3.py
+++ Assignment_3.py
@@ -1,1 +1,22 @@
 
+def inputMyName():
+    myName = input("What is your name? ")
+    return myName
+
+def inputMyAge():
+    inputAge = input("What is your age? ")
+    return inputAge
+
+def inputMyAddress():
+    inputAge = input("What is your address? ")
+    return inputAge
+
+def personalInfo(myName, myAge, myAddress):
+    print(f'Hi, my name is {myName} and I am {myAge} years old. I live in {myAddress}.')
+
+myName = inputMyName()
+myAge = inputMyAge()
+myAddress = inputMyAddress()
+personalInfo(myName, myAge, myAddress)
+print("I wish you all the best summoner")
+
