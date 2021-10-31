d13eea088016028ebc6f9ec8b70e9d15d66012a7
josephh27 <josephjasonsbuhain@iskolarngbayan.pup.edu.ph>
2021-10-31 20:47:36 +0800

    Assign_3.0

--- Assignment_3.py
+++ Assignment_3.py
@@ -1,23 +1,23 @@
 
 def inputMyName():
-    myName = input("What is your name? ")
+    myName = input("What is your name? ") #This is where the name goes
     return myName
 
 def inputMyAge():
-    inputAge = input("What is your age? ")
+    inputAge = input("What is your age? ") #This is where the age goes
     return inputAge
 
 def inputMyAddress():
-    inputAge = input("What is your address? ")
+    inputAge = input("What is your address? ") #This is where the address goes
     return inputAge
 
 def personalInfo(myName, myAge, myAddress):
-    print(f'Hi, my name is {myName} and I am {myAge} years old. I live in {myAddress}.')
+    print(f'Hi, my name is {myName} and I am {myAge} years old. I live in {myAddress}.') #This is where the accumulated information is printed out in a sentence manner
 
 myName = inputMyName()
 myAge = inputMyAge()
 myAddress = inputMyAddress()
 personalInfo(myName, myAge, myAddress)
 print("I wish you all the best summoner")
-print("I wish one more commit added")
+print("I wish one more commit added") #This one is when I just tried to add committing
 
